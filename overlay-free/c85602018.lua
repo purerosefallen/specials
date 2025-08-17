70bed1a746983cd8b6c2e28dae2f6872b0cca323
Vury Leo <i@vuryleo.com>
2025-08-17 22:35:59 +0900

    Revise Last Will

--- overlay-free/c85602018.lua
+++ overlay-free/c85602018.lua
@@ -1,73 +1,85 @@
 --遺言状
-function c85602018.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c85602018.target)
-	e1:SetOperation(c85602018.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	if not c85602018.global_check then
-		c85602018.global_check=true
+	if not s.global_check then
+		s.global_check=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_TO_GRAVE)
-		ge1:SetOperation(c85602018.checkop)
+		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
 	end
 end
-function c85602018.cfilter(c,tp)
+
+function s.cfilter(c,tp)
 	return c:IsControler(tp) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
 end
-function c85602018.checkop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	for p=0,1 do
-		if eg:IsExists(c85602018.cfilter,1,nil,p) then
-			Duel.RegisterFlagEffect(p,85602018,RESET_PHASE+PHASE_END,0,1)
+		if eg:IsExists(s.cfilter,1,nil,p) then
+			Duel.RegisterFlagEffect(p,id,RESET_PHASE+PHASE_END,0,1)
 		end
 	end
 end
-function c85602018.target(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	if Duel.GetFlagEffect(tp,85602018)~=0 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	if Duel.GetFlagEffect(tp,id)~=0 then
 		e:SetLabel(1)
 	else
-		e:SetCategory(0)
 		e:SetLabel(0)
 	end
 end
-function c85602018.activate(e,tp,eg,ep,ev,re,r,rp)
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c85602018.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-		and Duel.SelectYesNo(tp,aux.Stringid(85602018,0)) then
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c85602018.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			Duel.RegisterFlagEffect(p,85602019,RESET_PHASE+PHASE_END,0,1)
-  end
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_FREE_CHAIN)
-		--e1:SetCountLimit(1)
-		e1:SetCondition(c85602018.spcon)
-		e1:SetOperation(c85602018.spop)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,id+o,RESET_PHASE+PHASE_END,0,1)
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	--e1:SetCountLimit(1)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	--- How many times do we registered this effect
+	Duel.RegisterFlagEffect(tp,id+o*2,RESET_PHASE+PHASE_END,0,1)
 end
-function c85602018.spfilter(c,e,tp)
+
+function s.spfilter(c,e,tp)
 	return c:IsAttackBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c85602018.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,85602018)>Duel.GetFlagEffect(tp,85602019)
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local gy_action_times=Duel.GetFlagEffect(tp,id)
+	local ss_action_times=Duel.GetFlagEffect(tp,id+o)
+	local registerd_times=Duel.GetFlagEffect(tp,id+o*2)
+	return registerd_times*gy_action_times>ss_action_times
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c85602018.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
 end
-function c85602018.spop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_CARD,0,85602018)
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c85602018.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			Duel.RegisterFlagEffect(p,85602019,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(tp,id+o,RESET_PHASE+PHASE_END,0,1)
 end
