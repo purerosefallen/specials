6109bfe27d7e6b43760b37c482d6c0ddbf8822a4
Vury Leo <i@vuryleo.com>
2025-09-15 10:47:14 +0900

    Add 4gx (#177)

---
+++ 4GX/c23434538.lua
@@ -0,0 +1,71 @@
+--増殖するG
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.drcon1)
+	e1:SetOperation(s.drop1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	--sp_summon effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.regcon)
+	e2:SetOperation(s.regop)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetCondition(s.drcon2)
+	e3:SetOperation(s.drop2)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
+end
+function s.filter(c,sp)
+	return c:IsSummonPlayer(sp)
+end
+function s.drcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,1-tp)
+		and not Duel.IsChainSolving()
+end
+function s.drop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1*DrawOverride.VALUE,REASON_EFFECT)
+end
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,1-tp)
+		and Duel.IsChainSolving()
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
+end
+function s.drcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,id)>0
+end
+function s.drop2(e,tp,eg,ep,ev,re,r,rp)
+	local n=Duel.GetFlagEffect(tp,id)
+	Duel.ResetFlagEffect(tp,id)
+	Duel.Draw(tp,n*DrawOverride.VALUE,REASON_EFFECT)
+end
