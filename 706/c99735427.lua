60e4f1b92d2529864a38f6c634a71c30785c3114
301zrg <1627406938@qq.com>
2026-05-12 16:52:12 +0800

    光道武僧（44178886）洗回被禁忌的圣杯（25789292）无效的反转怪，那个反转怪的效果应该被无效 光道武僧（44178886）洗回光或者暗属性反转怪且场上有吸入闪光的魔镜（53341729）、吸入阴暗的魔镜（99735427）时，那个反转怪的效果应该被无效

---
+++ 706/c99735427.lua
@@ -0,0 +1,25 @@
+--暗闇を吸い込むマジック・ミラー
+---@param c Card
+function c99735427.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--disable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetOperation(c99735427.disop)
+	c:RegisterEffect(e2)
+end
+function c99735427.disop(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	-- 临时添加被武僧洗回卡组的反转效果无效
+	local c=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_EFFECT):GetHandler()
+	if re:IsActiveType(TYPE_MONSTER) and (loc==LOCATION_MZONE or loc==LOCATION_GRAVE or (loc==LOCATION_DECK and c:GetFlagEffect(44178886)>0))
+		and re:GetHandler():IsAttribute(ATTRIBUTE_DARK) then
+		Duel.NegateEffect(ev)
+	end
+end
